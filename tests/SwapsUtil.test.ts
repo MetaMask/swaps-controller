12876124609b8eaf95fe4cf0a775e939a5fad342
Esteban Mino <efmino@uc.cl>
2021-03-02 18:23:52 -0300

    tests

--- tests/SwapsUtil.test.ts
+++ tests/SwapsUtil.test.ts
@@ -313,19 +313,19 @@ describe('SwapsUtil', () => {
 
   describe('calculateGasLimits', () => {
     const gasEstimateWithRefund = '0x3c';
-    const gasEstimate = '0x64';
     const averageGas = 20;
     const maxGas = 40;
     const gasMultiplier = 1.2;
     it('if approval is needed, maxGas is the limit and averageGas is the estimated fee', () => {
-      const { tradeGasLimit, tradeMaxGasLimit } = swapsUtil.calculateGasLimits(true, gasEstimate, gasEstimateWithRefund, averageGas, maxGas, gasMultiplier);
+      const { tradeGasLimit, tradeMaxGasLimit } = swapsUtil.calculateGasLimits(true, gasEstimateWithRefund, averageGas, maxGas, gasMultiplier);
       expect(tradeGasLimit.toString()).toEqual(averageGas.toString());
       expect(tradeMaxGasLimit.toString()).toEqual(maxGas.toString());
     });
     it('if no approval is needed, gas limit is gas limit minus refund and max gas is the gas estimated by multiplier', () => {
-      const { tradeGasLimit, tradeMaxGasLimit } = swapsUtil.calculateGasLimits(false, gasEstimate, gasEstimateWithRefund, averageGas, maxGas, gasMultiplier);
-      expect(tradeGasLimit.toString(16)).toEqual(new BigNumber(gasEstimateWithRefund).times(gasMultiplier).toString(16));
-      expect(tradeMaxGasLimit.toString(16)).toEqual(new BigNumber(gasEstimate).times(gasMultiplier).toString(16));
+      const { tradeGasLimit, tradeMaxGasLimit } = swapsUtil.calculateGasLimits(false, gasEstimateWithRefund, averageGas, maxGas, gasMultiplier);
+      const limit: BigNumber = new BigNumber(gasEstimateWithRefund);
+      expect(tradeGasLimit.toString(16)).toEqual(limit.toString(16));
+      expect(tradeMaxGasLimit.toString(16)).toEqual(limit.times(gasMultiplier).toString(16));
     });
 
   });
