7235118ade658097b073b190fda7759b55e9692a
Pedro Pablo Aste Kompen <wachunei@gmail.com>
2021-04-22 14:14:40 -0400

    Remove console log from index

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
 import SwapsController from './SwapsController';
 
-export default SwapsController;
 export * as swapsUtils from './SwapsUtil';
-
-console.log('using swaps version 3.0.0');
+export default SwapsController;
