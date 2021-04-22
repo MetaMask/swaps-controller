925bd87d8f2e2c46b4e18e375326e1a83d18a847
Pedro Pablo Aste Kompen <wachunei@gmail.com>
2021-04-21 22:27:40 -0400

    Fix default export

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
-export default './SwapsController';
+import SwapsController from './SwapsController';
+
+export default SwapsController;
 export * as swapsUtils from './SwapsUtil';
 
 console.log('using swaps version 3.0.0');
