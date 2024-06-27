32bfa85ed045dc0d03557d16ed029d8feba33dbf
IF <139582705+infiniteflower@users.noreply.github.com>
2024-06-27 11:14:11 -0400

    chore: use BigNumber instead of bigint

--- src/SwapsController.ts
+++ src/SwapsController.ts
@@ -543,9 +543,9 @@ export default class SwapsController extends BaseControllerV1<
   private async getERC20Allowance(
     contractAddress: string,
     walletAddress: string,
-  ): Promise<bigint | unknown> {
+  ): Promise<BigNumber> {
     const contract = new this.web3.eth.Contract(abiERC20, contractAddress);
-    const allowanceTimeout = new Promise((_, reject) => {
+    const allowanceTimeout = new Promise<BigNumber>((_, reject) => {
       setTimeout(() => {
         reject(new Error(SwapsError.SWAPS_ALLOWANCE_TIMEOUT));
       }, 10000);
@@ -555,7 +555,7 @@ export default class SwapsController extends BaseControllerV1<
       const result: bigint = await contract.methods
         .allowance(walletAddress, getSwapsContractAddress(this.config.chainId))
         .call();
-      return result;
+      return new BigNumber(result.toString());
     };
 
     return Promise.race([allowanceTimeout, allowancePromise()]);
@@ -741,10 +741,7 @@ export default class SwapsController extends BaseControllerV1<
 
         // On Android, trying to cast a massive BigInt to a number will result in null
         // allowance and sourceAmount are in Solidity atomic amounts, so they can be bigger than a JS Number
-        if (
-          typeof allowance === 'bigint' &&
-          allowance < BigInt(fetchParams.sourceAmount)
-        ) {
+        if (allowance.isLessThan(new BigNumber(fetchParams.sourceAmount))) {
           approvalTransaction =
             quotesArray.find((quote) => quote.approvalNeeded)?.approvalNeeded ??
             null;
