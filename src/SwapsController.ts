eb253bde088ed175abe4fef54d1d6aa2ede32e5c
infiniteflower <139582705+infiniteflower@users.noreply.github.com>
2025-04-01 13:32:00 +0900

    feat: allow using a chainId or networkClientId to fetch top assets (#403)
    ## Explanation
    
    <!--
    Thanks for your contribution! Take a moment to answer these questions so
    that reviewers have the information they need to properly understand
    your changes:
    
    * What is the current state of things and why does it need to change?
    * What is the solution your changes offer and how does it work?
    * Are there any changes whose purpose might not obvious to those
    unfamiliar with the domain?
    * If your primary goal was to update one package but you found you had
    to update another one along the way, why did you do so?
    * If you had to upgrade a dependency, why did you do so?
    -->
    
    This PR allows passing in either a `chainId` or a `networkClientId` to
    `fetchTopAssets`. Previously only `networkClientId` was allowed.
    
    This is to support the case of Bridge where a user could select a
    destination chain that they have not added to MM yet, there by not
    having a `networkClientId` to provide. The user will have a destination
    `chainId` that we want to look up top assets on.
    
    ## References
    
    <!--
    Are there any issues that this pull request is tied to?
    Are there other links that reviewers should consult to understand these
    changes better?
    Are there client or consumer pull requests to adopt any breaking
    changes?
    
    For example:
    
    * Fixes #12345
    * Related to #67890
    -->
    
    Related to https://github.com/MetaMask/metamask-mobile/pull/14258
    
    ## Changelog
    
    <!--
    If you're making any consumer-facing changes, list those changes here as
    if you were updating a changelog, using the template below as a guide.
    
    (CATEGORY is one of BREAKING, ADDED, CHANGED, DEPRECATED, REMOVED, or
    FIXED. For security-related issues, follow the Security Advisory
    process.)
    
    Please take care to name the exact pieces of the API you've added or
    changed (e.g. types, interfaces, functions, or methods).
    
    If there are any breaking changes, make sure to offer a solution for
    consumers to follow once they upgrade to the changes.
    
    Finally, if you're only making changes to development scripts or tests,
    you may replace the template below with "None".
    -->
    
    ### `@metamask/swaps-controller`
    
    - CHANGED: Allow passing in either a `chainId` or a `networkClientId` to
    `fetchTopAssets`. Previously only `networkClientId` was allowed.
    
    ## Checklist
    
    - [x] I've updated the test suite for new or updated code as appropriate
    - [x] I've updated documentation (JSDoc, Markdown, etc.) for new or
    updated code as appropriate
    - [x] I've highlighted breaking changes using the "BREAKING" category
    above as appropriate
    - [x] I've prepared draft pull requests for clients and consumer
    packages to resolve any breaking changes

--- src/SwapsController.ts
+++ src/SwapsController.ts
@@ -1013,23 +1013,34 @@ export default class SwapsController extends BaseController<
    * Fetches the top assets and updates the state with them.
    * @param args - The arguments to this method.
    * @param args.networkClientId - The ID of a network client from
-   * NetworkController.
+   * NetworkController. This or chainId is required.
+   * @param args.chainId - The chain ID to fetch the top assets for. This or
+   * networkClientId is required. chainId will be preferred over networkClientId.
    */
   async fetchTopAssetsWithCache({
     networkClientId,
+    chainId,
   }: {
-    networkClientId: NetworkClientId;
+    networkClientId?: NetworkClientId;
+    chainId?: Hex;
   }) {
-    const chainId = this.#getChainId(networkClientId);
+    let chainIdToUse;
+    if (chainId) {
+      chainIdToUse = chainId;
+    } else if (networkClientId) {
+      chainIdToUse = this.#getChainId(networkClientId);
+    } else {
+      throw new Error('One of networkClientId or chainId is required');
+    }
 
-    if (!this.#supportedChainIds.includes(chainId)) {
+    if (!this.#supportedChainIds.includes(chainIdToUse)) {
       return;
     }
 
     const { topAssets } = this.state;
 
     const topAssetsLastFetchedForChain =
-      this.state.chainCache[chainId]?.topAssetsLastFetched ?? 0;
+      this.state.chainCache[chainIdToUse]?.topAssetsLastFetched ?? 0;
 
     const isPastThresholdForChain =
       this.#fetchTopAssetsThreshold < Date.now() - topAssetsLastFetchedForChain;
@@ -1037,7 +1048,7 @@ export default class SwapsController extends BaseController<
     if (!topAssets || isPastThresholdForChain) {
       const releaseLock = await this.#mutex.acquire();
       try {
-        const newTopAssets = await fetchTopAssets(chainId, this.#clientId);
+        const newTopAssets = await fetchTopAssets(chainIdToUse, this.#clientId);
         const data = {
           topAssets: newTopAssets,
           topAssetsLastFetched: Date.now(),
@@ -1046,7 +1057,7 @@ export default class SwapsController extends BaseController<
           _state.topAssets = data.topAssets;
           _state.chainCache = getNewChainCache(
             _state.chainCache,
-            chainId,
+            chainIdToUse,
             data,
           );
         });
@@ -1055,7 +1066,7 @@ export default class SwapsController extends BaseController<
         this.update((_state) => {
           _state.chainCache = getNewChainCache(
             _state.chainCache,
-            chainId,
+            chainIdToUse,
             data,
           );
         });
